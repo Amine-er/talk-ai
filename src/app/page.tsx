2a84b8d089e8a8797ab0c165c165f57cd25d8b72
Errabi Mohamed Amine <errabi.ami@gmail.com>
2025-08-16 00:27:06 +0100

    feat: implement authentication with better-auth and create user session management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,97 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+'use client';
+import { authClient } from '@/lib/auth-client';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
 
-const page = () => {
+const Home = () => {
+  const { data: session } = authClient.useSession();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email,
+        password,
+        name,
+      },
+      {
+        onSuccess: () => {
+          window.alert('User created successfully!');
+        },
+        onError: () => {
+          window.alert('Error creating user. Please try again.');
+        },
+      }
+    );
+  };
+
+  const onLogin = () => {
+    authClient.signIn.email(
+      {
+        email,
+        password,
+      },
+      {
+        onSuccess: () => {
+          window.alert('User created successfully!');
+        },
+        onError: () => {
+          window.alert('Error creating user. Please try again.');
+        },
+      }
+    );
+  };
+
+  if (session) {
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        <p>Logged in as {session.user.name}</p>
+        <Button onClick={() => authClient.signOut()}>Sign Out</Button>
+      </div>
+    );
+  }
+
   return (
-    <Button variant={'destructive'}> Click me </Button>
-  )
-}
+    <div className="flex flex-col gap-y-10">
+      <div className="flex flex-col items-center justify-center h-screen space-y-4">
+        <Input
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={onSubmit}>Create User</Button>
+      </div>
+      <div className="flex flex-col items-center justify-center h-screen space-y-4">
+        <Input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={onLogin}>Login</Button>
+      </div>
+    </div>
+  );
+};
 
-export default page 
+export default Home;
